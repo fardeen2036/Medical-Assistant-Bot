6c27faa622a1f4bcd1585e74a9b70dc24b22e3b9
Fardeen Pathan <167503530+fardeen2036@users.noreply.github.com>
2025-05-20 17:25:36 +0530

    testing .py

--- testing .py
+++ testing .py
@@ -1,7 +1,5 @@
 import redis
 
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
-
-# Add sample interaction for test
 r.sadd("drug:acetaminophen:interactions", "ibuprofen|Avoid combining due to increased risk of liver toxicity")
 r.sadd("drug:aspirin:interactions", "ibuprofen|May reduce aspirin's effectiveness")
